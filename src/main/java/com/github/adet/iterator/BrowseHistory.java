3be271c03aa891581ad1ed58cb6a7d0ac3680d71
Adet <adet@Adetias-MacBook-Pro.local>
2024-05-06 14:28:16 +0700

    iterator pattern - array

--- src/main/java/com/github/adet/iterator/BrowseHistory.java
+++ src/main/java/com/github/adet/iterator/BrowseHistory.java
@@ -5,39 +5,40 @@ import java.util.List;
 
 public class BrowseHistory {
 
-    private List<String> urls = new ArrayList<>();
+    private String[] urls = new String[10];
+
+    private int size;
 
     public void push(String url) {
-        urls.add(url);
+        urls[size++] = url;
     }
 
     public String pop() {
-        int lastIndex = urls.size() - 1;
-        return urls.remove(lastIndex);
+        return urls[--size];
     }
 
     public Iterator createIterator() {
-        return new ListIterator(this);
+        return new ArrayInterator(this);
     }
 
-    public class ListIterator implements Iterator {
+    public class ArrayInterator implements Iterator {
 
         private final BrowseHistory history;
 
         private int index;
 
-        public ListIterator(BrowseHistory history) {
+        public ArrayInterator(BrowseHistory history) {
             this.history = history;
         }
 
         @Override
         public boolean hasNext() {
-            return index < history.urls.size();
+            return index < history.size;
         }
 
         @Override
         public String current() {
-            return history.urls.get(index);
+            return history.urls[index];
         }
 
         @Override
